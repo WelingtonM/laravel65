b799d68cf058263c6ee6d62593720b62f7abbfdc
Welington Miyazato <welingtonmiyazato@gmail.com>
2019-12-15 17:59:27 +0900

    Friendly router POST params forms

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,20 +16,41 @@ Route::get('/', function () {
 });
 
 Route::get('client', function(){
-    return "Hello Client!";
+    $csrfToken = csrf_token();
+    $html = <<<HTML
+<html>
+<body>
+<h3>Client</h3>
+<form action="/client/register" method="post">
+<input type="hidden" name="_token" value="$csrfToken" />
+<input type="text" name="name" /><br />
+<button type="submit">Submint</button>
+</form>
+</body>
+</html>
+HTML;
+    return $html;
+    
 });
-Route::get('/admin/client', function(){
-    return "Admin - Hello client!";
-});
-Route::get('/client-echo', function(){
-    echo "Text by echo!";
-});
-Route::get('/product/{name}/{id}', function($name, $id){
-    print "Product: # {$id}";
+
+
+Route::post('/client/register', function(Request $request){
+    echo $request->get('name');
     echo "<br />";
-    print "Product: Name {$name}";
+    echo $request->name;
 });
-Route::get('/fornecedor/{name}/{id?}', function($name, $id = 1000){
-    return "Fornecedor: {$name} - #{$id}";
-});
+// Route::get('/admin/client', function(){
+//     return "Admin - Hello client!";
+// });
+// Route::get('/client-echo', function(){
+//     echo "Text by echo!";
+// });
+// Route::get('/product/{name}/{id}', function($name, $id){
+//     print "Product: # {$id}";
+//     echo "<br />";
+//     print "Product: Name {$name}";
+// });
+// Route::get('/fornecedor/{name}/{id?}', function($name, $id = 1000){
+//     return "Fornecedor: {$name} - #{$id}";
+// });
 
